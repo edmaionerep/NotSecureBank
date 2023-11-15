1818c8ce1fabb8437c209d3199b0cb5d5cf2a55b
Edoardo Maione <ed.maione@reply.it>
2023-11-15 18:49:10 +0100

    Fix V11_SessionManagement

--- src/com/notsecurebank/util/OperationsUtil.java
+++ src/com/notsecurebank/util/OperationsUtil.java
@@ -27,22 +27,9 @@ public class OperationsUtil {
 
         try {
             Long accountId = -1L;
-            Cookie[] cookies = request.getCookies();
-
-            Cookie notSecureBankCookie = null;
-
-            for (Cookie cookie : cookies) {
-                if (ServletUtil.NOT_SECURE_BANK_COOKIE.equals(cookie.getName())) {
-                    notSecureBankCookie = cookie;
-                    break;
-                }
-            }
-
-            Account[] cookieAccounts = null;
-            if (notSecureBankCookie == null)
-                cookieAccounts = user.getAccounts();
-            else
-                cookieAccounts = Account.fromBase64List(notSecureBankCookie.getValue());
+            
+            // Allow to use ONLY logged-User's accounts to be "debitActId"
+            Account[] notCookieAccounts = user.getAccounts();
 
             try {
                 accountId = Long.parseLong(accountIdString);
@@ -52,14 +39,14 @@ public class OperationsUtil {
             }
 
             if (accountId > 0) {
-                for (Account account : cookieAccounts) {
+                for (Account account : notCookieAccounts) {
                     if (account.getAccountId() == accountId) {
                         debitActId = account.getAccountId();
                         break;
                     }
                 }
             } else {
-                for (Account account : cookieAccounts) {
+                for (Account account : notCookieAccounts) {
                     if (account.getAccountName().equalsIgnoreCase(accountIdString)) {
                         debitActId = account.getAccountId();
                         break;
